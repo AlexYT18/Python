cdad376f36a7d3008b99d18e910e7da2e265a0b1
Alex <v1136v@gmail.com>
2024-10-15 21:18:51 +0300

    laba1
    laba 1

---
+++ Course Python английский/Базовые типы языка Python и работа с ними/Составные типы данных/Явное приведение типов в Python/main.py
@@ -0,0 +1,6 @@
+str_ = "sdkfjsdkjfnksjdnfkjsdnfkjsndfkjsndfkjsndfkjsnd"
+unique_chars = set(str_)
+#print(unique_chars)
+number_of_unique_chars = len(unique_chars)
+print(number_of_unique_chars)
+# TODO найдите и распечатайте количество уникальных символов в строке
