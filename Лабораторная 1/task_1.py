7fe2495e0d5d53b9b5c11a1a4bf582f87fd627ee
Alex <v1136v@gmail.com>
2024-10-25 18:08:04 +0300

    tvar

--- Лабораторная 1/task_1.py
+++
@@ -1,4 +0,0 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-unum = [i for i in numbers if i is not None]
-numbers[numbers.index(None)] = round(sum(unum) / (len(unum) + 1), 5)
-print("Измененный список:", numbers)
