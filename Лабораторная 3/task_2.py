703317101cf900ddd08a1ba5254645af1ea5c6c2
Alex <59798556+AlexYT18@users.noreply.github.com>
2024-11-07 20:42:41 +0300

    Update task_2.py

--- Лабораторная 3/task_2.py
+++ Лабораторная 3/task_2.py
@@ -2,7 +2,7 @@ def find_common_participants(group1, group2, delimiter=','):
     participants_first_group = set(group1.split(delimiter))
     participants_second_group = set(group2.split(delimiter))
     common_participants = participants_first_group.intersection(participants_second_group)
-    return sorted(common_participants)
+    return list(common_participants)
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
