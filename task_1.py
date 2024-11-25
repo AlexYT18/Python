b782c90cf2fc33510c8c714ffded24a5c915da1f
Alex <59798556+AlexYT18@users.noreply.github.com>
2024-11-25 17:56:08 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,13 +1,12 @@
 import json
 
+
 def task() -> float:
     with open("input.json", "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
-    total_sum = 0.0
-    for entry in data:
-        score = entry.get("score", 0)
-        weight = entry.get("weight", 0)
-        total_sum += score * weight
+    total_sum = sum(entry.get("score", 0) * entry.get("weight", 0) for entry in data)
+
     return round(total_sum, 3)
+
 
 print(task())
